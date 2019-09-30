3c6aba9ec39a0b2cb56f2e1465585d9afe45cea9
Middley <amiddley@gmail.com>
2019-09-30 08:30:30 -0500

    mi aporte en clase

---
+++ src/github_proyect/GitHub_proyect.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github_proyect;
+
+/**
+ *
+ * @author MIDDLEY
+ */
+public class GitHub_proyect {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
